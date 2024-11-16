1b6dfae78d7993e74fae04289709d200a5e2da13
Morten Bek Ditlevsen <morten@ka-ching.dk>
2024-11-16 12:49:26 +0100

    More food

--- thelittlegame.swiftpm/DigitalPet.swift
+++ thelittlegame.swiftpm/DigitalPet.swift
@@ -50,6 +50,34 @@ let carrot: [Int] = [
     0, 1, 1, 0, 0, 0, 0, 0, 0, 0,
 ]
 
+let sushi: [Int] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 0, 1, 2, 2, 3, 3, 1, 0, 0,
+    0, 0, 1, 2, 2, 3, 3, 1, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+let icecream: [Int] = [
+    0, 0, 0, 2, 2, 2, 0, 0, 0, 0,
+    0, 0, 3, 3, 3, 3, 3, 0, 0, 0,
+    0, 0, 2, 2, 2, 2, 2, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 1, 0, 0, 0,
+    0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+    0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+]
+
+let fish: [Int] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 1, 1, 1, 1, 0, 0, 1, 0,
+    0, 1, 0, 1, 1, 1, 1, 1, 0, 0,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 0, 1, 1, 1, 1, 0, 0, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+]
+
+
 struct Size {
     let width: Int
     let height: Int
@@ -87,7 +115,7 @@ struct Image {
             }
         }
     }
-
+    
 }
 
 let catImage = Image(buffer: cat, palette: Palette(colors: [
@@ -123,14 +151,21 @@ let carrotImage = Image(buffer: carrot, palette: Palette(colors: [
     .green,
 ]))
 
-let cakeImage = Image(buffer: carrot, palette: Palette(colors: [
-    .init(r: 0, g: 8, b: 0),
-    .init(r: 0, g: 16, b: 0),
+let icecreamImage = Image(buffer: icecream, palette: Palette(colors: [
+    .orange,
+    .pink,
+    .init(r: 8, g: 0, b: 8),
 ]))
 
-let sushiImage = Image(buffer: carrot, palette: Palette(colors: [
-    .init(r: 0, g: 8, b: 16),
-    .init(r: 0, g: 16, b: 0),
+let fishImage = Image(buffer: fish, palette: Palette(colors: [
+    .lightBlue,
+]))
+
+
+let sushiImage = Image(buffer: sushi, palette: Palette(colors: [
+    .lightWhite,
+    .pink,
+    .green,
 ]))
 
 enum PetType: Int {
@@ -138,7 +173,7 @@ enum PetType: Int {
     case cat = 1
     case panda = 2
     case unicorn = 3
-//case heart = 4
+    //case heart = 4
     
     var image: Image {
         switch self {
@@ -154,37 +189,46 @@ enum PetType: Int {
 struct Pet {
     let petType: PetType
     mutating func eat(food: Food, at time: Double) {
-        switch (petType, food) {
-        case (.rabbit, .carrot),
-             (.unicorn, .cake),
-            (.panda, .sushi):
-            self.happyness += 1
-            self.time = time
+        //switch (petType, food) {
+        //case (.rabbit, .carrot),
         
-        default:
-            ()
-        }
+        // (.unicorn, .icecream),
+        // (.panda, .sushi):
+        self.happyness += 1
+        self.showHeart = true
+        self.time = time
+        
+        //default:
+        //  ()
+        //}
     }
     mutating func tick(_ now: Double) {
+        if now - time > 1.9, showHeart {
+            showHeart = false
+            // time = 0
+        }
         if now - time > 2, happyness > 0 {
+            time = now
             happyness -= 1
-            time = 0
         }
     }
     var happyness: Int = 0
     var time: Double = 0
+    var showHeart: Bool = false
 }
 
 enum Food: Int {
     case carrot = 0
-    case cake = 1
+    case icecream = 1
     case sushi = 2
+    case fish = 3
     
     var image: Image {
         switch self {
         case .carrot: carrotImage
-        case .cake: cakeImage
+        case .icecream: icecreamImage
         case .sushi: sushiImage
+        case .fish: fishImage
         }
     }
 }
@@ -195,6 +239,7 @@ func createDigitalPetLevel() -> Level {
     var time: Double = 0
     return Level { screen, t in
         time = t
+        screen.red = Double(pet.happyness) / 5
         for x in 0 ..< screen.width {
             for y in 0 ..< screen.height {
                 let image: Image
@@ -227,7 +272,7 @@ func createDigitalPetLevel() -> Level {
             } else {
                 pet = Pet(petType: PetType(rawValue: pet.petType.rawValue + 1) ?? .rabbit)
             }
-         case .select:
+        case .select:
             ()
         }
     }    
